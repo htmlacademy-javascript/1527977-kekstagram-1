7ca21037e0be068da5bdd515ba2d53b2004d98a1
lemurlene <lenagorelikova4560@gmail.com>
2024-04-03 15:39:25 +0300

    feat: рендерит карточки

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-import './createPictures.js';
+import {renderCards} from './createPictures.js';
+import {createPhotos} from './data.js';
 
+const PHOTOS_COUNT = 25;
+
+const photos = createPhotos(PHOTOS_COUNT);
+
+renderCards(photos);
